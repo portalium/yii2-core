a7b5b6ce3499c489c3e370812be3331fe39b9a37
demirmehmet0 <demirmehmet0@yahoo.com>
2024-01-20 14:06:11 +0300

    fix: change style padding

--- grid/ActionColumn.php
+++ grid/ActionColumn.php
@@ -15,17 +15,17 @@ class ActionColumn extends \yii\grid\ActionColumn
     {
         $this->initDefaultButton('view', 'eye-open', [
             'class' => 'btn btn-info btn-xs',
-            'style' => 'padding: 2px 9px 2px 9px;'
+            'style' => 'padding: 2px 9px 2px 9px; display: inline-block;'
         ]);
         $this->initDefaultButton('update', 'pencil', [
             'class' => 'btn btn-primary btn-xs',
-            'style' => 'padding: 2px 9px 2px 9px;'
+            'style' => 'padding: 2px 9px 2px 9px; display: inline-block;'
         ]);
         $this->initDefaultButton('delete', 'trash', [
             'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
             'data-method' => 'post',
             'class' => 'btn btn-danger btn-xs',
-            'style' => 'padding: 2px 9px 2px 9px;'
+            'style' => 'padding: 2px 9px 2px 9px; display: inline-block;'
         ]);
 
         parent::initDefaultButtons();
