3658c4679e5c6900c774dd6b7cc83d4ab66d4aed
demirmehmet0 <demirmehmet0@yahoo.com>
2024-02-28 14:51:24 +0300

    feat: dataProvider wrapped

--- data/ActiveDataProvider.php
+++ data/ActiveDataProvider.php
@@ -13,20 +13,24 @@ class ActiveDataProvider extends \yii\data\ActiveDataProvider
 
     protected function prepareModels()
     {
-        if(!Yii::$app->request->get('per-page') && isset($this->pagination->pageSize)  && $this->pagination != false && $this->pagination->pageSize == 20) {
+        $pageParam = 'per-page';
+        if (!isset(Yii::$app->request->queryParams[$pageParam])) {
+            $pageParam = 'dp-1-per-page';
+        }
+        if(!Yii::$app->request->get('per-page') && isset($this->pagination->pageSize)  && $this->pagination != false) {
             $sessionPageSize = Yii::$app->session->get('theme::page_size');
             $this->pagination->pageSize = !$sessionPageSize ? Yii::$app->setting->getValue('theme::page_size') : $sessionPageSize;
             Yii::$app->session->set('theme::page_size', $this->pagination->pageSize);
         }
-        if ($this->pagination != false && $this->pagination->getPageSize() && isset($_GET['per-page'])) {
+        if ($this->pagination != false && $this->pagination->getPageSize() && isset(Yii::$app->request->queryParams[$pageParam])) {
             $sessionPageSize = Yii::$app->session->get('theme::page_size');
             if ($sessionPageSize == null) {
                 $sessionPageSize = Yii::$app->setting->getValue('theme::page_size');
             }
-            $this->pagination->pageSize = isset(Yii::$app->request->queryParams['per-page']) ? Yii::$app->request->queryParams['per-page'] : $sessionPageSize;
+            $this->pagination->pageSize = isset(Yii::$app->request->queryParams[$pageParam]) ? Yii::$app->request->queryParams[$pageParam] : $sessionPageSize;
             Yii::$app->session->set('theme::page_size', $this->pagination->pageSize);
         }
-
+//         Yii::warning('theme::page_size: ' . $this->pagination->pageSize);
 //        Yii::$app->session->set('theme::page_size', $this->pagination->pageSize);
         return parent::prepareModels();
     }
